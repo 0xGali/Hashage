7721fcd0bd6ce017cb12763b3c55242072597dfc
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-07 16:55:19 +0200

    ajout de xorStringBinaire et sigma0

--- src/main/java/com/blockchain/sha256/TraitementMot.java
+++ src/main/java/com/blockchain/sha256/TraitementMot.java
@@ -13,9 +13,28 @@ public class TraitementMot {
         String motN0="";
         String motN="";
         for(int i=0;i<nbDecal;i++){
-            motN0 = motN0+"0";
-            motN = motN+mot.charAt(i);
+            motN0 += "0";
+            motN += mot.charAt(i);
         }
         return motN0+motN;
+    }
+
+    static String  xorStringBinaire(String mot1, String mot2){
+        String xor = "";
+        
+        // Boucle qui compare chaque caractère des mots
+        for (int i = 0; i < mot1.length(); i++)
+        {
+            //On vérifie si les caractères sont les mêmes
+            if (mot1.charAt(i) == mot2.charAt(i))
+                xor += "0";
+            else
+                xor += "1";
+        }
+        return xor;
+    }
+
+    public static String sigma0(String mot){
+        return xorStringBinaire(xorStringBinaire(rotation(mot,7) , rotation(mot, 18)),decalage(mot, 3));
     }
 }
