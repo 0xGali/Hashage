2ef67f2766973f9efb0f4f6a3be9f614e965e13d
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-09 18:34:05 +0200

    Ajout de la méthode sigma1 et creerMots

--- src/main/java/com/blockchain/sha256/TraitementMot.java
+++ src/main/java/com/blockchain/sha256/TraitementMot.java
@@ -6,4 +6,16 @@ public class TraitementMot {
         return Operations.xorBinaryStrings(Operations.xorBinaryStrings(Operations.rotation(mot,7) ,
                 Operations.rotation(mot, 18)),Operations.decalage(mot, 3));
     }
+
+    public static String sigma1(String mot){
+        return Operations.xorBinaryStrings(Operations.xorBinaryStrings(Operations.rotation(mot,17) ,
+                Operations.rotation(mot, 19)),Operations.decalage(mot, 10));
+    }
+
+    public static String[] creerMots(String[] mots){
+        for(int index=16;index<64;index++){
+            mots[index]=sigma1(mots[index-2])+mots[index-7]+sigma0(mots[index-15])+mots[index-16];
+        }
+        return mots;
+    }
 }
