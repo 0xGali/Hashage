5dcd58770bda2e9bb799dd6963d0118f043158a6
Maximilien Bleeker <maximilien.bleeker9@etu.univ-lorraine.fr>
2025-10-09 13:53:54 +0200

    Ajustement suite à la fusion des branches

--- src/main/java/com/blockchain/sha256/Main.java
+++ src/main/java/com/blockchain/sha256/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(Hashage.Sha265("abc"));
-        System.out.println(TraitementMot.decalage("111111",3));
+        System.out.println(Operations.decalage("111111",3));
         System.out.println(101^010);
     }
 }
--- src/main/java/com/blockchain/sha256/TraitementMot.java
+++ src/main/java/com/blockchain/sha256/TraitementMot.java
@@ -6,23 +6,4 @@ public class TraitementMot {
         return Operations.xorBinaryStrings(Operations.xorBinaryStrings(Operations.rotation(mot,7) ,
                 Operations.rotation(mot, 18)),Operations.decalage(mot, 3));
     }
-
-    static String  xorStringBinaire(String mot1, String mot2){
-        String xor = "";
-        
-        // Boucle qui compare chaque caractère des mots
-        for (int i = 0; i < mot1.length(); i++)
-        {
-            //On vérifie si les caractères sont les mêmes
-            if (mot1.charAt(i) == mot2.charAt(i))
-                xor += "0";
-            else
-                xor += "1";
-        }
-        return xor;
-    }
-
-    public static String sigma0(String mot){
-        return xorStringBinaire(xorStringBinaire(rotation(mot,7) , rotation(mot, 18)),decalage(mot, 3));
-    }
 }
